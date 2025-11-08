32a0ef01d022e9eee9cbc180516ac90ecadb0cf7
Lionel Kamto <kamtolionel@gmail.com>
2025-11-08 05:18:07 +0100

    Add missing Bigger-italic font

--- app/fonts.ts
+++ app/fonts.ts
@@ -6,8 +6,5 @@ export const bigger = localFont({
   preload: true,
   src: [
     { path: "../public/fonts/bigger/BiggerDisplay.otf", weight: "900", style: "normal" },
-    { path: "../public/fonts/bigger/Bigger-italic.ttf", weight: "900", style: "italic" },
-    { path: "../public/fonts/bigger/BiggerBook.ttf", weight: "400", style: "normal" },
-    { path: "../public/fonts/bigger/BiggerBook-italic.ttf", weight: "400", style: "italic" },
   ],
 })
