686219a4121b23cdaa71f36b81e41fba24c75d36
COD3VIL <73862378+michaelnji@users.noreply.github.com>
2025-11-25 16:36:50 +0100

    refactor: refactor projects section components for improved structure and performance

--- app/projets/page.tsx
+++ app/projets/page.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import { FlipCountdownSection } from "@/components/sections/flipcountdown-section"
+import ProjectsListSection from "@/components/sections/projects-list-section";
 
 export default function ProjetsPage() {
-  return <FlipCountdownSection />
+  return <ProjectsListSection />;
 }
 
